05f716c1a44f79556e9e308b2aebf101f060c4d4
Alegne <lensalegne@gmail.com>
2021-04-30 05:43:02 +0300

    deploy v2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,9 @@
 export const environment = {
   production: false,
 
-  // endpoint: "http://127.0.0.1:8000/api",
-  // url: "http://127.0.0.1:8000/api",
-  endpoint: "http://toor.cactus.eni.mg/api",
-  url: "http://toor.cactus.eni.mg/api"
+
+  endpoint: "http://site.cactus.eni.mg/api",
+  url: "http://site.cactus.eni.mg/api"
 
 
 };
